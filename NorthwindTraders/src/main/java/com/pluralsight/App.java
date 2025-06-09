96a6b5bf29e1b1b692eb16a5502e9fd87061c330
Alondra Gamez <agamez@appdev.yearup.org>
2025-06-09 14:50:49 -0500

    added a way to not show your username and password on Northwind Traders

--- NorthwindTraders/src/main/java/com/pluralsight/App.java
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -10,12 +10,21 @@ public class App {
         System.out.println("║   Products   ║");
         System.out.println("╚══════════════╝");
 
+        if (args.length != 2) {
+            System.out.println("Application needs two arguments to run: " + "java com.pluralsight.UsingDriverManager <username> <password>");
+            System.exit(1);
+        }
+
+        // get the username and password form the command line args
+        String username = args[0];
+        String password = args[1];
+
         try {
 
             // 1. open a connection to the database
             // use the database URL to point to the correct database
             Connection connection;
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "L@l@t!n@5");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
 
             // create statement
             // the statement is tied to the open connection
